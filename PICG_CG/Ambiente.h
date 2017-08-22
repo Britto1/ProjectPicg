0273a23d14fff9789fb76400fc2c3c198e99252f
Britto1 <paulobritodemoc@hotmail.com>
2017-08-21 21:48:05 -0300

    Update Ambiente.h

--- PICG_CG/Ambiente.h
+++ PICG_CG/Ambiente.h
@@ -2,10 +2,25 @@
 #define AMBIENTE_H
 
 
+
+
+
+#include <vector>
+
+
+using namespace std;
+
+
 class Ambiente
 {
+    float sizeIlha;
+
     public:
-        Ambiente();
+        Ambiente(float sizeIlha); //em porcentagem
+        void criarIlha(vector<vector<char>> &mundo);
+        void criarOceano(vector<vector<char>> &mundo);
+        void desenharAmbiente(vector<vector<char>> mundo);
+        int getAreaI(vector<vector<char>> mundo);
         virtual ~Ambiente();
         Ambiente(const Ambiente& other);
         Ambiente& operator=(const Ambiente& other);
